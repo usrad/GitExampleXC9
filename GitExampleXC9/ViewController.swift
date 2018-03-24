8f38faf985b1c815571ff535ea7ebee52905ef18
us <u@usretina>
2018-03-24 13:56:21 +0100

    Added print statement

--- GitExampleXC9/ViewController.swift
+++ GitExampleXC9/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("some change")
     }
 
     override var representedObject: Any? {
